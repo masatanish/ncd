5263a4a40e342cd5af0ac1caf4629518caa649dc
masatanish <masatanish@gmail.com>
2013-05-01 11:20:34 +0900

    implement ncd calculation

--- lib/ncd.rb
+++ lib/ncd.rb
@@ -1,5 +1,16 @@
+require 'zlib'
 require "ncd/version"
 
-module Ncd
-  # Your code goes here...
+module NCD
+  def self.normalized_compression_distance(a, b)
+    min, max = [a, b].map{|d| Zlib::Deflate.deflate(d, 9).size }.minmax
+    cab = Zlib::Deflate.deflate(a+b, 9).size
+    (cab - min) / max.to_f
+  end
+end
+
+class String
+  def ncd(b)
+    NCD.normal_compression_distance(self, b)
+  end
 end
