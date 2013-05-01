214b6aa05614c0f74a9361ac3cc4c0dbcc3c5a76
masatanish <masatanish@gmail.com>
2013-05-01 13:12:41 +0900

    mod

--- lib/ncd.rb
+++ lib/ncd.rb
@@ -2,10 +2,9 @@ require 'zlib'
 require_relative 'ncd/version'
 
 module NCD
-  @@level = Zlib::BEST_COMPRESSION
   def self.distance(a, b)
-    min, max = [a, b].map{|d| Zlib::Deflate.deflate(d, @@level).size }.minmax
-    cab = Zlib::Deflate.deflate(a+b, @@level).size
+    min, max = [a, b].map{|d| Zlib::Deflate.deflate(d, Zlib::BEST_COMPRESSION).size }.minmax
+    cab = Zlib::Deflate.deflate(a+b, Zlib::BEST_COMPRESSION).size
     (cab.to_f - min.to_f) / max.to_f
   end
 end
